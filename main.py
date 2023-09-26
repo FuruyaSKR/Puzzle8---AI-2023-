7109db6ea93619b26eb1818065243bdf77a01e96
Victor Hugo Z <victorhugo@offertech.com.br>
2023-09-25 23:47:57 -0300

    Primary Modified: Changed all code

--- main.py
+++ main.py
@@ -1,183 +1,125 @@
-from heapq import heappop, heappush
-import math
 import numpy as np
 import time
 import random
-import heapq
 
 
-def f_x(puzzle):
-    return g_x(puzzle) + h_x(puzzle)
-
-
-def g_x(puzzle):
-    goal = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
-    count = 0
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] != goal[i][j]:
-                count += puzzle[i][j]
-    return count
-
-
-def h_x(puzzle):
-    goal = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
-    distance = 0
-    for i in range(3):
-        for j in range(3):
-            if puzzle[i][j] != goal[i][j]:
-                value = puzzle[i][j]
-                goal_i, goal_j = np.where(np.array(goal) == value)
-                distance += dist_manhattan([i, j], [goal_i[0], goal_j[0]])
-    return distance
+GOAL = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
+SOLUCION = False
 
 
 def dist_manhattan(x, y):
     return sum([abs(xi - yi) for xi, yi in zip(x, y)])
 
 
-# def move_piece(puzzle, move):
-#     empty_i, empty_j = np.where(np.array(puzzle) == 0)
-
-#     if move == "cima":
-#         if empty_i > 0:
-#             puzzle[empty_i][empty_j], puzzle[empty_i - 1][empty_j] = (
-#                 puzzle[empty_i - 1][empty_j],
-#                 puzzle[empty_i][empty_j],
-#             )
-#     elif move == "baixo":
-#         if empty_i < 2:
-#             puzzle[empty_i][empty_j], puzzle[empty_i + 1][empty_j] = (
-#                 puzzle[empty_i + 1][empty_j],
-#                 puzzle[empty_i][empty_j],
-#             )
-#     elif move == "esquerda":
-#         if empty_j > 0:
-#             puzzle[empty_i][empty_j], puzzle[empty_i][empty_j - 1] = (
-#                 puzzle[empty_i][empty_j - 1],
-#                 puzzle[empty_i][empty_j],
-#             )
-#     elif move == "direita":
-#         if empty_j < 2:
-#             puzzle[empty_i][empty_j], puzzle[empty_i][empty_j + 1] = (
-#                 puzzle[empty_i][empty_j + 1],
-#                 puzzle[empty_i][empty_j],
-#             )
-
-#     return puzzle
+def evaluation_function(state):
+    h_x = 0
+    g_x = 0
+    for i in range(len(GOAL)):
+        for j in range(len(GOAL[i])):
+            if state[i][j] != GOAL[i][j] and state[i][j] != 0:
+                value = state[i][j]
+                goal_i, goal_j = np.where(np.array(GOAL) == value)
+                h_x += dist_manhattan([i, j], [goal_i[0], goal_j[0]])
+                g_x += 1
+    f_x = g_x + h_x
+    return f_x, g_x, h_x
 
 
-def move_function(puzzle):
-    empty_tile = np.where(puzzle == 0)
-    empty_i, empty_j = empty_tile[0][0], empty_tile[1][0]
+def move(puzzle):
+    void_position = np.where(puzzle == 0)
+    v_x, v_y = void_position
+    v_x = int(v_x)
+    v_y = int(v_y)
 
-    moves = []
-    if empty_i > 0:
-        new_puzzle = puzzle.copy()
-        new_puzzle[empty_i, empty_j] = new_puzzle[empty_i - 1, empty_j]
-        new_puzzle[empty_i - 1, empty_j] = 0
-        moves.append(new_puzzle)
+    # Evaluating moves in x
+    moves_x = 2 if v_x != 0 and v_x != len(puzzle) - 1 else 1
+    moves_y = 2 if v_y != 0 and v_y != len(puzzle[0]) - 1 else 1
 
-    if empty_i < 2:
-        new_puzzle = puzzle.copy()
-        new_puzzle[empty_i, empty_j] = new_puzzle[empty_i + 1, empty_j]
-        new_puzzle[empty_i + 1, empty_j] = 0
-        moves.append(new_puzzle)
+    expended_nodes = moves_x + moves_y
 
-    if empty_j > 0:
-        new_puzzle = puzzle.copy()
-        new_puzzle[empty_i, empty_j] = new_puzzle[empty_i, empty_j - 1]
-        new_puzzle[empty_i, empty_j - 1] = 0
-        moves.append(new_puzzle)
+    possible_moves = []
 
-    if empty_j < 2:
-        new_puzzle = puzzle.copy()
-        new_puzzle[empty_i, empty_j] = new_puzzle[empty_i, empty_j + 1]
-        new_puzzle[empty_i, empty_j + 1] = 0
-        moves.append(new_puzzle)
+    for i in range(moves_x):
+        branch = puzzle.copy()
+        if v_x == 0:
+            branch[v_x][v_y] = puzzle[v_x + 1][v_y]
+            branch[v_x + 1][v_y] = puzzle[v_x][v_y]
+        elif v_x == len(puzzle) - 1:
+            branch[v_x][v_y] = puzzle[v_x - 1][v_y]
+            branch[v_x - 1][v_y] = puzzle[v_x][v_y]
+        else:
+            if i == 0:
+                branch[v_x][v_y] = puzzle[v_x + 1][v_y]
+                branch[v_x + 1][v_y] = puzzle[v_x][v_y]
+            else:
+                branch[v_x][v_y] = puzzle[v_x - 1][v_y]
+                branch[v_x - 1][v_y] = puzzle[v_x][v_y]
+        possible_moves.append(branch)
 
-    return moves
+    for j in range(moves_y):
+        branch = puzzle.copy()
+        if v_y == 0:
+            branch[v_x][v_y] = puzzle[v_x][v_y + 1]
+            branch[v_x][v_y + 1] = puzzle[v_x][v_y]
+        elif v_y == len(puzzle) - 1:
+            branch[v_x][v_y] = puzzle[v_x][v_y - 1]
+            branch[v_x][v_y - 1] = puzzle[v_x][v_y]
+        else:
+            if j == 0:
+                branch[v_x][v_y] = puzzle[v_x][v_y + 1]
+                branch[v_x][v_y + 1] = puzzle[v_x][v_y]
+            else:
+                branch[v_x][v_y] = puzzle[v_x][v_y - 1]
+                branch[v_x][v_y - 1] = puzzle[v_x][v_y]
+        possible_moves.append(branch)
+    for index, candidate in enumerate(possible_moves):
+        fx, gx, hx = evaluation_function(candidate)
+        print(
+            f"""
+Candidate: \n{candidate}
+f_x: {fx}
+g_x: {gx}
+h_x: {hx}
+"""
+        )
+        print("=" * 20)
+        if index == 0 or fx < best_move[1]:
+            best_move = (index, fx, candidate)
+
+    return (expended_nodes, best_move[-1])
 
 
 def astar(puzzle):
-    # Inicialização
-    open_set = []
-    closed_set = set()
-    heapq.heappush(open_set, (f_x(puzzle), puzzle))
+    final_state = False
+    total_nodes = 0
+    count = 0
+    while not final_state and count < 100:
+        print(f"""Move: {count} \nAvaliando: \n{puzzle}""")
+        nodes, puzzle = move(puzzle)
+        total_nodes += nodes
 
-    while open_set:
-        # Seleciona o estado com menor valor de f_x()
-        current_state = heapq.heappop(open_set)[1]
-
-        # Verifica se é o estado objetivo
-        if is_goal_state(current_state):
-            return current_state
-
-        # Adiciona o estado atual ao conjunto fechado
-        closed_set.add(tuple(map(tuple, current_state)))
-
-        # Gera os possíveis movimentos
-        moves = move_function(current_state)
-
-        for move in moves:
-            # Verifica se o estado já foi visitado
-            if tuple(map(tuple, move)) in closed_set:
-                continue
-
-            # Calcula os valores f_x(), g_x() e h_x() para o novo estado
-            f = f_x(move)
-            g = g_x(move)
-            h = h_x(move)
-            print(f"Função de g_x(): {g}, Função de h_x(): {h}")
-            # Adiciona o novo estado à lista de estados abertos
-            heapq.heappush(open_set, (f, move))
-
-    return None
-
-
-def is_goal_state(puzzle):
-    goal = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
-    return np.array_equal(puzzle, goal)
-
-
-def main():
-    start_time = time.time()
-
-    numbers = list(range(9))
-    random.shuffle(numbers)
-    puzzle = np.array(numbers).reshape((3, 3))
-
-    goal = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
-    num_nodes = 0
-
-    while not np.array_equal(puzzle, goal):
-        # Escolha um movimento aleatório
-        moves = ["cima", "baixo", "esquerda", "direita"]
-        move = random.choice(moves)
-
-        # Faça o movimento escolhido
-        puzzle = move_piece(puzzle, move)
-
-        # Incremente o número de nodos na árvore de busca
-        num_nodes += 1
-
-        # Imprima os resultados
-        print(f"Quebra-cabeça resolvido em {num_nodes} nodos.")
-
-    # Adicione o timer para verificar o tempo de execução
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Tempo de execução: {execution_time} segundos")
+        final_state = (puzzle == GOAL).all() == True
+        count += 1
+    SOLUCION = bool(count < 100)
+    return total_nodes, puzzle
 
 
 if __name__ == "__main__":
     numbers = list(range(9))
     random.shuffle(numbers)
     initial_puzzle = np.array(numbers).reshape((3, 3))
-    solution = astar(initial_puzzle)
-
-    if solution is None:
+    start = time.time()
+    # initial_puzzle = np.array([[2, 6, 3], [0, 1, 4], [8, 7, 5]])
+    print("=" * 20)
+    print(f"""Puzzle inicial:\n {initial_puzzle}\n""")
+    print("=" * 20)
+    total_nodes, solution = astar(initial_puzzle)
+    end = time.time()
+    if not SOLUCION:
         print("Nenhuma solução encontrada.")
     else:
-        print("Solução encontrada:")
-        print(solution)
+        print("Solução encontrada:\n")
+        print(solution, "\n")
+    print("=" * 20)
+    print(f"Nodes: {total_nodes}")
+    print(f"Search time: {end-start:4f}s")
